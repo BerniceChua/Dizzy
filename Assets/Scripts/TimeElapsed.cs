ade848957c958df7ec63e3083a7d32ddb1e05924
BerniceChua <Bernice.Chua.415@gmail.com>
2017-05-31 15:26:38 -0700

    Create intro/splash screen.

--- Assets/Scripts/TimeElapsed.cs
+++ Assets/Scripts/TimeElapsed.cs
@@ -1,16 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TimeElapsed : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    [SerializeField] Text m_timeElapsed;
+    //float time = Time.realtimeSinceStartup;
+    int m_timer = 0;
+
+    // Use this for initialization
+    void Start () {
+        m_timeElapsed.text = "00:00";
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        int minutes = Mathf.FloorToInt(m_timer / 60);
+        int seconds = Mathf.FloorToInt(m_timer % 60);
+
+        string formattedTime = string.Format("{0:0}:{1:00}", minutes, seconds);
+
+        m_timeElapsed.text = formattedTime;
+    }
 }
