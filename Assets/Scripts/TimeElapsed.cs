e79215edae1d10b2957cc298ffe3103dbe8e396c
BerniceChua <Bernice.Chua.415@gmail.com>
2017-06-01 17:13:12 -0700

    Add splash screen/intro screen.

--- Assets/Scripts/TimeElapsed.cs
+++ Assets/Scripts/TimeElapsed.cs
@@ -22,5 +22,10 @@ public class TimeElapsed : MonoBehaviour {
         string formattedTime = string.Format("{0:0}:{1:00}", minutes, seconds);
 
         m_timeElapsed.text = formattedTime;
+
+        System.TimeSpan t = System.TimeSpan.FromSeconds(m_timer);
+        string timerFormatted = string.Format("{0:D2}h:{1:D2}m:{2:D2}s:{3:D3}ms", t.Hours, t.Minutes, t.Seconds, t.Milliseconds);
+
+        m_timeElapsed.text = timerFormatted;
     }
 }
