70a27f15ee6eec20834e05baa171c40312e4809c
BerniceChua <Bernice.Chua.415@gmail.com>
2017-06-05 18:54:07 -0700

    Create ScoreTime.cs to handle scorekeeping logic.

---
+++ Assets/Scripts/ScoreTime.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreTime : MonoBehaviour {
+    bool m_isNewScoreHigher = false;
+
+    //// Use this for initialization
+    //void Start () {
+
+    //}
+
+    //// Update is called once per frame
+    //void Update () {
+
+    //}
+
+    public float ShowCurrentHighScore(string scoreName) {
+        return PlayerPrefs.GetFloat(scoreName);
+    }
+
+    public void FindHigherScore(string scoreName, float oldScore, float newScore) {
+        if (oldScore >= newScore)
+            return;
+
+        m_isNewScoreHigher = true;
+        PlayerPrefs.SetFloat(scoreName, newScore);
+    }
+
+    public bool IsNewScoreHigher() {
+        return m_isNewScoreHigher;
+    }
+
+}
