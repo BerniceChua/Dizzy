31649dcbfa432d43805c2f94ae255f270281f30a
BerniceChua <Bernice.Chua.415@gmail.com>
2017-06-11 01:53:18 -0700

    Create RepositionAfterGameOver.cs so that the game object will be in front of the camera when the game is reset; and made the Game Piece a child object of the camera.

---
+++ Assets/Scripts/RepositionAfterGameOver.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RepositionAfterGameOver : MonoBehaviour {
+    [SerializeField] TimeElapsed m_timeElapsed;
+    [SerializeField] GameObject m_gamePiece;
+
+    private Vector3 m_initialPosition;
+    private Quaternion m_initialRotation;
+
+    // Use this for initialization
+    void Start () {
+        m_initialRotation = m_gamePiece.transform.rotation;
+        m_initialPosition = new Vector3(0, -5, 44.77f);
+    }
+
+    // Update is called once per frame
+    void Update () {
+        //if (m_timeElapsed.m_gameOver) {
+        //    m_gamePiece.SetActive(false);
+        //    m_gamePiece.transform.position.Set(0, -5, 44.77f);
+        //}
+    }
+
+    public void ResetPosition() {
+        //if (m_timeElapsed.m_gameOver) {
+        //m_gamePiece.transform.position.Set(0, -5, 44.77f);
+        m_gamePiece.transform.position = m_initialPosition;
+        m_gamePiece.transform.rotation = m_initialRotation;
+        m_gamePiece.SetActive(false);
+    //}
+    }
+
+    public void ReEnable() {
+        m_gamePiece.SetActive(true);
+    }
+}
